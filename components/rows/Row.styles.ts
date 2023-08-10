f62037de184a1735674b2f2d3712996bb3e91cde
KJHeartbreaker <kay.jay.obrien@gmail.com>
2023-08-09 19:06:34 -0600

    debugging height

--- components/rows/Row.styles.ts
+++ components/rows/Row.styles.ts
@@ -89,6 +89,7 @@ export const TwoColumnRowContainer = styled(ContentContainer)`
 		z-index: 1;
 		width: 100%;
 		height: 100%;
+		border: 2px solid springgreen;
 
 		@media ${device.sm} {
 			align-items: center;
