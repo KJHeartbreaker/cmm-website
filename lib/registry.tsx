c79fe344922b317f6c5f0c0c2cf949c7b2d8a8da
KJHeartbreaker <kay.jay.obrien@gmail.com>
2023-10-18 20:46:17 -0600

    Cleared styled components error

--- lib/registry.tsx
+++ lib/registry.tsx
@@ -5,6 +5,7 @@
 import { useServerInsertedHTML } from 'next/navigation'
 import React, { useState } from 'react'
 import { ServerStyleSheet, StyleSheetManager } from 'styled-components'
+import isPropValid from '@emotion/is-prop-valid'
 
 export default function StyledComponentsRegistry({ children }: { children: React.ReactNode }) {
 	// Only create stylesheet once with lazy initial state
@@ -18,10 +19,23 @@ export default function StyledComponentsRegistry({ children }: { children: React
 		return styles
 	})
 
+	// This implements the default behavior from styled-components v5
+	const shouldForwardProp = (propName, target) => {
+		if (typeof target === 'string') {
+			// For HTML elements, forward the prop if it is a valid HTML attribute
+			return isPropValid(propName)
+		}
+		// For other elements, forward all props
+		return true
+	}
+
 	if (typeof window !== 'undefined') return <>{children}</>
 
 	return (
-		<StyleSheetManager sheet={styledComponentsStyleSheet.instance}>
+		<StyleSheetManager
+			sheet={styledComponentsStyleSheet.instance}
+			shouldForwardProp={shouldForwardProp}
+		>
 			{children}
 		</StyleSheetManager>
 	)
